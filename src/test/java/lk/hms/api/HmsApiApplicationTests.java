63805a978cc4b8f3f2a6e219ea0c7bceee848611
IT21178986-Nimesha Madurangi <116791096+NimeshaMadurangi@users.noreply.github.com>
2025-01-17 17:34:41 +0530

    first commit

---
+++ src/test/java/lk/hms/api/HmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.hms.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
